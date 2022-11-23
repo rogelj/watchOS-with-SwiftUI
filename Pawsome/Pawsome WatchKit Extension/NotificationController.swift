eb165dfdce41b730aca20efe61a1c512c0a824f7
J Rogel <jrogel.datascience@icloud.com>
2022-11-23 14:49:53 +0000

    Notification

--- Pawsome/Pawsome WatchKit Extension/NotificationController.swift
+++ Pawsome/Pawsome WatchKit Extension/NotificationController.swift
@@ -3,8 +3,11 @@ import SwiftUI
 import UserNotifications
 
 class NotificationController: WKUserNotificationHostingController<NotificationView> {
+  var image: Image!
+  var message: String!
+
   override var body: NotificationView {
-    return NotificationView()
+    return NotificationView(message: message, image: image)
   }
 
   override func willActivate() {
@@ -21,5 +24,11 @@ class NotificationController: WKUserNotificationHostingController<NotificationVi
     // This method is called when a notification needs to be presented.
     // Implement it if you use a dynamic notification interface.
     // Populate your dynamic notification interface as quickly as possible.
+
+    let content = notification.request.content
+    message = content.body
+
+    let num = Int.random(in: 1...20)
+    image = Image("cat\(num)")
   }
 }
--- Pawsome/Pawsome WatchKit Extension/NotificationView.swift
+++ Pawsome/Pawsome WatchKit Extension/NotificationView.swift
@@ -1,13 +1,29 @@
 import SwiftUI
 
 struct NotificationView: View {
+    // 1
+  let message: String
+  let image: Image
+
+    // 2
   var body: some View {
-    Text("Hello, World!")
+    ScrollView {
+      Text(message)
+        .font(.headline)
+
+      image
+        .resizable()
+        .scaledToFit()
+    }
   }
 }
 
 struct NotificationView_Previews: PreviewProvider {
   static var previews: some View {
-    NotificationView()
+      // 3
+    NotificationView(
+      message: "Awww",
+      image: Image("cat\(Int.random(in: 1...20))")
+    )
   }
 }
