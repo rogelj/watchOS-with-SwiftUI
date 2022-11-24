d128b59e25d0a16f2133444d5c0bdc0c303edaf0
J Rogel <jrogel.datascience@icloud.com>
2022-11-24 11:20:55 +0000

    Configuring the descriptior

--- TideWatch/TideWatch WatchKit Extension/Complications/ComplicationController.swift
+++ TideWatch/TideWatch WatchKit Extension/Complications/ComplicationController.swift
@@ -2,6 +2,22 @@ import ClockKit
 
 class ComplicationController: NSObject, CLKComplicationDataSource {
   func currentTimelineEntry(for complication: CLKComplication) async -> CLKComplicationTimelineEntry? {
-    return nil
+    guard complication.family == .circularSmall else {
+      return nil
+    }
+
+    let template = CLKComplicationTemplateGraphicCircularStackText(
+      line1TextProvider: .init(format: "Surf's"),
+      line2TextProvider: .init(format: "Up!")
+      )
+
+    return .init(date: Date(), complicationTemplate: template)
+  }
+
+  func complicationDescriptors() async -> [CLKComplicationDescriptor] {
+    return [
+      .init(identifier: "com.raywenderlich.TideWatch", displayName: "Tide Conditions", supportedFamilies: [.graphicCircular]
+           )
+    ]
   }
 }
