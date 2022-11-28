8cc9932797a8937ebdc41cbb7c817548058326c6
J Rogel <jrogel.datascience@icloud.com>
2022-11-28 15:32:56 +0000

    URL Downloads

--- Updates/Updates WatchKit Extension/ExtensionDelegate.swift
+++ Updates/Updates WatchKit Extension/ExtensionDelegate.swift
@@ -2,6 +2,8 @@ import WatchKit
 import ClockKit
 
 final class ExtensionDelegate: NSObject, WKExtensionDelegate {
+  private let backgroundWorker = BackgroundWorker()
+
   public static func updateActiveComplications() {
     let server = CLKComplicationServer.sharedInstance()
     server.activeComplications?.forEach {
@@ -12,6 +14,15 @@ final class ExtensionDelegate: NSObject, WKExtensionDelegate {
   func handle(_ backgroundTasks: Set<WKRefreshBackgroundTask>) {
     backgroundTasks.forEach { task in
       switch task {
+        case let task as WKApplicationRefreshBackgroundTask:
+          backgroundWorker.perform { updateComplications in
+            if updateComplications {
+              Self.updateActiveComplications()
+            }
+
+            backgroundWorker.schedule()
+            task.setTaskCompletedWithSnapshot(false)
+          }
         default:
           task.setTaskCompletedWithSnapshot(false)
       }
