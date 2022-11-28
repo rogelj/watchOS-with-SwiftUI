bb945e81b65cde25991d7ad0109352962ba63c1e
J Rogel <jrogel.datascience@icloud.com>
2022-11-28 15:38:08 +0000

    Dowloader

--- Updates/Updates WatchKit Extension/ExtensionDelegate.swift
+++ Updates/Updates WatchKit Extension/ExtensionDelegate.swift
@@ -3,6 +3,7 @@ import ClockKit
 
 final class ExtensionDelegate: NSObject, WKExtensionDelegate {
   private let backgroundWorker = BackgroundWorker()
+  private var downloads: [String: UrlDownloader] = [:]
 
   public static func updateActiveComplications() {
     let server = CLKComplicationServer.sharedInstance()
@@ -23,9 +24,27 @@ final class ExtensionDelegate: NSObject, WKExtensionDelegate {
             backgroundWorker.schedule()
             task.setTaskCompletedWithSnapshot(false)
           }
+        case let task as WKURLSessionRefreshBackgroundTask:
+          let downloder = downloader(for: task.sessionIdentifier)
+          downloder.perform { updateComplications in
+            if updateComplications {
+              Self.updateActiveComplications()
+            }
+            downloder.schedule()
+            task.setTaskCompletedWithSnapshot(false)
+          }
         default:
           task.setTaskCompletedWithSnapshot(false)
       }
     }
+  }
+
+  private func downloader(for identifier: String) -> UrlDownloader {
+    guard let download = downloads[identifier] else {
+      let downloader = UrlDownloader(identifier: identifier)
+      downloads[identifier] = downloader
+      return downloader
+    }
+    return download
   }
 }
