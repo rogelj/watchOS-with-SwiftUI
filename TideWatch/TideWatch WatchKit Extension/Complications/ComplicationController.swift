43486f7a8c63addc6d6e7f8c1a1af89376249bdc
J Rogel <jrogel.datascience@icloud.com>
2022-11-28 11:24:31 +0000

    Sample data in complication picker

--- TideWatch/TideWatch WatchKit Extension/Complications/ComplicationController.swift
+++ TideWatch/TideWatch WatchKit Extension/Complications/ComplicationController.swift
@@ -1,6 +1,17 @@
 import ClockKit
 
 class ComplicationController: NSObject, CLKComplicationDataSource {
+  func complicationDescriptors() async -> [CLKComplicationDescriptor] {
+    return [
+      .init(identifier: "com.raywenderlich.TideWatch",
+            displayName: "Tide Conditions",
+            supportedFamilies: [
+              .graphicCircular
+            ]
+           )
+    ]
+  }
+
   func currentTimelineEntry(for complication: CLKComplication) async -> CLKComplicationTimelineEntry? {
     guard complication.family == .circularSmall else {
       return nil
@@ -9,15 +20,25 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     let template = CLKComplicationTemplateGraphicCircularStackText(
       line1TextProvider: .init(format: "Surf's"),
       line2TextProvider: .init(format: "Up!")
-      )
+    )
 
     return .init(date: Date(), complicationTemplate: template)
   }
 
-  func complicationDescriptors() async -> [CLKComplicationDescriptor] {
-    return [
-      .init(identifier: "com.raywenderlich.TideWatch", displayName: "Tide Conditions", supportedFamilies: [.graphicCircular]
-           )
-    ]
+  func localizableSampleTemplate(for complication: CLKComplication) async -> CLKComplicationTemplate? {
+    guard complication.family == .graphicCircular,
+          let image = UIImage(named: "tide_rising") else {
+      return nil
+    }
+
+    let tide = Tide(entity: Tide.entity(), insertInto: nil)
+    tide.date = Date()
+    tide.height = 24
+    tide.type = .high
+
+    return CLKComplicationTemplateGraphicCircularStackImage(
+      line1ImageProvider: .init(fullColorImage: image),
+      line2TextProvider: .init(format: tide.heightString())
+      )
   }
 }
