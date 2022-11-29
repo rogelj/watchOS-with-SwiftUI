fcc86ec693509488640bcdbe173a23fb44a73825
J Rogel <jrogel.datascience@icloud.com>
2022-11-29 15:44:39 +0000

    More SIWA

--- SignIn/SignIn WatchKit Extension/Authenticating/SignInView.swift
+++ SignIn/SignIn WatchKit Extension/Authenticating/SignInView.swift
@@ -3,7 +3,10 @@ import AuthenticationServices
 
 struct SignInView: View {
   @AppStorage("userName") private var storedUserName = ""
+  @AppStorage("userCredential") private var userCredential = ""
   @Environment(\.dismiss) private var dismiss
+
+  private let nonce = UUID().uuidString
 
   let onSignedIn: (String) -> Void
   
@@ -39,10 +42,30 @@ struct SignInView: View {
   }
 
   private func onRequest(request: ASAuthorizationAppleIDRequest) {
-
+    request.requestedScopes = [.fullName]
+    request.state = "some state string"
+    request.nonce = nonce
   }
 
   private func siwaCompletion(result: Result<ASAuthorization, Error>) {
+    guard
+      case .success(let authorization) = result,
+      let credential = authorization.credential as? ASAuthorizationAppleIDCredential
+    else {
+      if case .failure(let error) = result {
+        print("Failed to authenticate: \(error.localizedDescription)")
+      }
+
+      return
+    }
+
+    if credential.fullName == nil {
+      // You've logged in an existing user account
+    } else {
+      // The user does not exist, so register a new account
+    }
+
+    userCredential = credential.user
 
   }
 }
