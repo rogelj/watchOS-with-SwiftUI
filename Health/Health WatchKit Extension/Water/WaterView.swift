4e4f2797f1f80a4e41fb529a2d61b6e895e7cd8c
J Rogel <jrogel.datascience@icloud.com>
2022-11-30 14:32:16 +0000

    Showing data to user

--- Health/Health WatchKit Extension/Water/WaterView.swift
+++ Health/Health WatchKit Extension/Water/WaterView.swift
@@ -1,43 +1,82 @@
-//
-//  WaterView.swift
-//  Health WatchKit App
-//
-//  Created by J Rogel PhD on 30/11/2022.
-//  Copyright © 2022 Ray Wenderlich. All rights reserved.
-//
+  //
+  //  WaterView.swift
+  //  Health WatchKit App
+  //
+  //  Created by J Rogel PhD on 30/11/2022.
+  //  Copyright © 2022 Ray Wenderlich. All rights reserved.
+  //
 
 import SwiftUI
 import HealthKit
 
 struct WaterView: View {
-    var body: some View {
-      ScrollView {
-        VStack {
-          if HealthStore.shared.isWaterEnabled {
-            Text("Add water")
-              .font(.headline)
+  @State private var consumed = ""
+  @State private var percent = ""
 
-            HStack {
-              LogWaterButton(size: .small) { logWater(quantity: $0) }
-              LogWaterButton(size: .large) { logWater(quantity: $0) }
-            }
-            .padding()
-          } else {
-            Text("Please enable water tracking in Apple Health.")
+  var body: some View {
+    ScrollView {
+      VStack {
+        if HealthStore.shared.isWaterEnabled {
+          Text("Add water")
+            .font(.headline)
+
+          HStack {
+            LogWaterButton(size: .small) { logWater(quantity: $0) }
+            LogWaterButton(size: .large) { logWater(quantity: $0) }
           }
+          .padding(.bottom)
+
+          HStack {
+            Text("Today:")
+              .font(.headline)
+            Text(consumed)
+              .font(.body)
+          }
+
+          HStack {
+            Text("Goal:")
+              .font(.headline)
+            Text(percent)
+              .font(.body)
+          }
+        } else {
+          Text("Please enable water tracking in Apple Health.")
         }
       }
     }
+    .task {
+      await updateStatus()
+    }
+  }
 
   private func logWater(quantity: HKQuantity) {
     Task {
       try await HealthStore.shared.logWater(quantity: quantity)
+      await updateStatus()
     }
   }
-}
 
-struct WaterView_Previews: PreviewProvider {
-    static var previews: some View {
-        WaterView()
+  private func updateStatus() async {
+    guard let (measurement, percent) = try? await HealthStore.shared.currentWaterStatus()
+    else {
+      consumed = "0"
+      percent = "Unknown"
+      return
     }
+
+    consumed = consumedFormat.string(from: measurement)
+
+    self.percent = percent?.formatted(.percent.precision(.fractionLength(0))) ?? "Unknown"
+  }
+
+  private let consumedFormat: MeasurementFormatter = {
+    var fmt = MeasurementFormatter()
+    fmt.unitOptions = .naturalScale
+    return fmt
+  }()
+}
+struct WaterView_Previews: PreviewProvider {
+  static var previews: some View {
+    WaterView()
+  }
 }
